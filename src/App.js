8f0c3290f522f8a4811d746ace6c837fed707be7
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-20 14:12:22 +0500

    React 16: Starter project files from treehouse

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-import StudentForm from './components/StudentForm';
-import ErrorBoundary from './components/ErrorBoundary';
+import Teacher from './Teacher';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            </header>
 
-            <ErrorBoundary>
-                <StudentForm />
-            </ErrorBoundary>
-        </div>
+  state = {
+    teacher: ''
+  };
+
+  updateTeacher = teacher => {
+    this.setState({ teacher });
+  }
+
+  render() {
+    const teachers = ['jay', 'vivianne', 'ecma', 'json'];
+
+    return (
+      <div className="App">
+        <header>
+          <h1>Meet the Teachers</h1>
+          {teachers.map((teacher,i) =>
+            <button
+              key={teacher}
+              type="button"
+              value={teacher}
+              onClick={e => this.updateTeacher(e.target.value)}
+            >{teacher}
+            </button>)}
+        </header>
+
+        <Teacher teacher={this.state.teacher} />
+      </div>
     );
   }
 }
