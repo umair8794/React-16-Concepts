565a80ef346fddeaf288d5ca01af6faea17a6760
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-20 14:08:59 +0500

    React 16: Portals example code

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import './App.css';
 
 import StudentForm from './components/StudentForm';
 import ErrorBoundary from './components/ErrorBoundary';
+import Modal from './components/Modal';
 
 class App extends Component {
+
+    handleClick() {
+        console.log("I was clicked");
+    }
+
   render() {
     return (
-        <div className="App">
+        <div className="App" onClick={this.handleClick}>
             <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
@@ -17,6 +23,13 @@ class App extends Component {
             <ErrorBoundary>
                 <StudentForm />
             </ErrorBoundary>
+
+            <Modal>
+                <div className="modal">
+                    This is the modal window
+                    <button>Close x</button>
+                </div>
+            </Modal>
         </div>
     );
   }
