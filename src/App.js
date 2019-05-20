73d75604b1ffe14926eefd40d018b9d34f415ef3
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-20 13:40:31 +0500

    React 16: New return types project files from treehouse

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import StudentForm from './components/StudentForm';
-import ErrorBoundary from './components/ErrorBoundary';
+import Header from './Header';
+import Teachers from './Teachers';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            </header>
-
-            <ErrorBoundary>
-                <StudentForm />
-            </ErrorBoundary>
-        </div>
+      <div className="App">
+        <Header />
+        <Teachers />
+        <Footer />
+      </div>
     );
   }
 }
--- src/error-config.js
+++
@@ -1,6 +0,0 @@
-// Include your error config here
-
-export function sendError(error, extra) {
-    console.log(error);
-    console.log(extra);
-}
